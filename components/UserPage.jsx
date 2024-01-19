2799ce3cbd9d0363347a9ddcf1ab2aedfa6e479a
SSSamedov <samedov-sultan@mail.ru>
2024-01-19 09:23:14 +0300

    about done

--- components/UserPage.jsx
+++ components/UserPage.jsx
@@ -2,19 +2,16 @@ const React = require('react')
 const Layout = require('./Layout')
 
 
-function UserPage({ title, user }){
-    return( <Layout title={title} user={user}>
-         <h1>Мой профиль</h1>
-       
-          <img src={user.img} className='card-img-top' alt='...' />
-          <div className='card-body'>
-            <h5 className='card-title'>{user.name}</h5>
-          </div>
-      
-
-    </Layout>)
+function UserPage({ title, user }) {
+  return (<Layout title={title} user={user}>
+    <h1>Мой профиль</h1>
+    <div className='profileContainer'>
+      <img src={user.img} className='userImage' alt='...' />
+      <h5 className='profileTitle'>Имя: {user.name}</h5>
+    </div>
+  </Layout>)
 
 }
 
-module.exports= UserPage
+module.exports = UserPage
 
