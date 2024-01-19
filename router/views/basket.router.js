177d157b7981324d1dc1a4088511e873d1144f3f
CanaveralEnvy <oldelevennn@gmail.com>
2024-01-19 09:42:04 +0300

    basketfinal

--- router/views/basket.router.js
+++ router/views/basket.router.js
@@ -23,19 +23,3 @@ router.get("/", async (req, res) => {
 
 module.exports = router;
 
-// const router = require('express').Router();
-// const Basket = require('../../components/Basket');
-// const { Order, OrderItem, Drug } = require('../../db/models');
-
-// router.get('/', async (req, res) => {
-//   const order = await Order.findAll({
-//     where: { status: 'не оплачен' },
-//     include: { model: OrderItem, include: { model: Drug } },
-//   });
-//   const html = res.renderComponent(Basket, {
-//     title: 'Basket',
-//     order,
-//   });
-//   res.send(html);
-// });
-// module.exports = router;
